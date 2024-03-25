d48ddb508c64b421b5937de73b41eb127427e07a
princefranklenstyle <princesinoia@icloud.com>
2024-03-25 14:02:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import React,{useState, useEffect} from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Kuda from './components/Kuda';
+import Donate from './Pages/Donate/Donate';
+import OurStory from './Pages/OurStory/OurStory';
+import InnovationHubs from './Pages/InnovationHubs/InnovationHubs';
+import HireUs from './Pages/HireUs/HireUs';
+import Blog from './components/Blog';
+import Home from './Pages/Home/Home'
+
+
+function App() {
+
+
+
+    
+  return (
+    <Router>
+          <Kuda />
+          <Routes>
+            <Route element={<Home />} path="/" />
+            <Route element={<OurStory />} path="/our-story" />
+            <Route element={<InnovationHubs />} path="/innovation-hubs" />
+            <Route element={<HireUs />} path="/hire-us" />
+            <Route element={<Donate />} path="/donate" />
+            <Route element={<Blog />} path="/blog" />
+
+            </Routes>
+
+      </Router>
+  );
+}
+
+export default App;
+
